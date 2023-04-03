757dd5ec9604dc9a251c4902d071929e47873c65
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2023-04-03 17:15:47 +0200

    periph.sram: for convenience init sram content from the constructor

--- lambdasoc/periph/sram.py
+++ lambdasoc/periph/sram.py
@@ -31,7 +31,7 @@ class SRAMPeripheral(Peripheral, Elaboratable):
         Wishbone bus interface.
     """
     # TODO raise bus.err if read-only and a bus write is attempted.
-    def __init__(self, *, size, data_width=32, granularity=8, writable=True):
+    def __init__(self, *, size, data_width=32, granularity=8, writable=True, init=None):
         super().__init__()
 
         if not isinstance(size, int) or size <= 0 or size & size-1:
@@ -42,7 +42,7 @@ class SRAMPeripheral(Peripheral, Elaboratable):
                              "of {} ({} / {})"
                               .format(size, data_width // granularity, data_width, granularity))
 
-        self._mem = Memory(depth=(size * granularity) // data_width, width=data_width)
+        self._mem = Memory(depth=(size * granularity) // data_width, width=data_width, init=init)
 
         self.bus = wishbone.Interface(addr_width=log2_int(self._mem.depth),
                                       data_width=self._mem.width, granularity=granularity,
